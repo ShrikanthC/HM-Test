cf0b45e1ad2a660ef1996f30709b486b995f7afc
ShrikanthC <harikanth94@gmail.com>
2019-12-31 07:49:37 +0530

    update flow

--- cab/common/models/driver-info.js
+++ cab/common/models/driver-info.js
@@ -2,8 +2,9 @@
 
 module.exports = function (Driverinfo) {
 
-    Driverinfo.create = (request, type, callback) => {
+    Driverinfo.saveOrUpdate = (request, callback) => {
         if (!request.id) {
+            //create flow
             Driverinfo.find({ "where": { "driverId": request.driverId }, "fields": ['carId'] }).then(driverData => {
                 if (driverData.length > 0) {
                     let error = new Error();
@@ -22,8 +23,17 @@ module.exports = function (Driverinfo) {
                 }
                 else {
                     Driverinfo.create(request, (err, driverCarInfo) => {
-                        return callback(null, { "message": "carr booked to driver successfully" })
+                        return callback(null, { "message": "carr assigned to driver successfully" })
                     })
+                }
+            })
+
+        } else {
+            //update flow
+            Driverinfo.find({ "where": { "cardId": request.carId, "isDeleted": 1 } }).then(carInactive => {
+                if (carInactive.length > 0) {
+                    Driverinfo.upsertWithWhere({ "driverId": request.driverId, "carId": request.carId, "isDeleted": 1 }, { "isDeleted": 0 });
+                    return callback(null, { "message": "driver updated the car sucessfully" });
                 }
             })
 
